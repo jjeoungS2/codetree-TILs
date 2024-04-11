47fd279c08454085a9957eb6ad5eecb392e5c8af
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-04-11 21:13:01 +0900

    Title: 메이즈 러너, Time: 91ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240411/메이즈 러너/maze-runner.java
+++ 240411/메이즈 러너/maze-runner.java
@@ -10,7 +10,7 @@ public class Main {
 	static int res = 0;
 	static int bx = 0, by = 0, bsize = 0;
 	
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // 여기에 코드를 작성해주세요.
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
