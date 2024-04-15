50170a61e5111e69dbdbbf6f218b6d2e3f7b9504
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-04-15 17:17:29 +0900

    Title: 두 번째로 작은 수의 위치, Time: 238ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240415/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[][] list = new int[n][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for(int i = 0; i < n; i++){
+            list[i][0] = Integer.parseInt(st.nextToken());
+            list[i][1] = i+1;
+        }
+
+        Arrays.sort(list,(o1,o2) -> {
+            return o1[0] - o2[0];
+        });
+        System.out.print(list[1][1]);
+    }
+}
