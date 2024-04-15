c6270e0dfb354703fa1e5a3489387ff24536b7a0
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-04-15 18:33:33 +0900

    Title: 두 번째로 작은 수의 위치, Time: 297ms, Memory: 14MB, Status: Passed - CodeTree

--- 240415/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.java
+++ 240415/두 번째로 작은 수의 위치/location-of-the-second-smallest-number.java
@@ -4,6 +4,11 @@ public class Main {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        
+        if(n == 1) {
+        	System.out.println("-1");
+        	return;
+        }
 
         int[][] list = new int[n][2];
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -16,14 +21,20 @@ public class Main {
         Arrays.sort(list,(o1,o2) -> {
             return o1[0] - o2[0];
         });
-    
+        
         int num = list[0][0];
-        for(int i = 1; i < n; i++) {
+        int cnt = 0;
+        int i = 1;
+        for(; i < n-1; i++) {
         	if(num == list[i][0]) continue;
         	else {
-                System.out.print(list[i][1]);
+                cnt = list[i][0];
                 break;
         	}
         }
+        if(list[i][0] == list[i-1][0])System.out.println("-1");
+        else if(i+1 >= n)System.out.println(list[i][1]);
+        else if(list[i+1][0] == cnt) System.out.println("-1");
+        else System.out.println(list[i][1]);
     }
 }
