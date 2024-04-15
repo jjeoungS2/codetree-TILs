4b63fe740c7a771d93d7eed5712ea8baf2d889fd
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-04-15 17:09:08 +0900

    Title: 순위 경쟁2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240415/순위 경쟁2/ranking-competition2.java
@@ -0,0 +1,51 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+    	static int A = 0;
+	static int B = 0;
+	static String win = "";
+	static int res = 0;
+    public static void main(String[] args) throws IOException{
+        der br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine());
+		String s = "";
+		int num = 0;
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		s = st.nextToken();
+		num = Integer.parseInt(st.nextToken());
+		if(s.equals("A")) {
+			A += num;
+		} else {
+			B += num;
+		}
+		if(A < B) win = "B";
+		else win = "A";
+		res++;
+		
+		for(int i = 0; i < n-1; i++) {
+			st = new StringTokenizer(br.readLine());
+			s = st.nextToken();
+			num = Integer.parseInt(st.nextToken());
+			if(s.equals("A")) A+=num;
+			else B+=num;
+			
+			if(A == B) {
+				if(!win.equals("AB")) {
+					res++;
+					win = "AB";
+				}
+			} else if(A > B) {
+				if(!win.equals("A")) {
+					res++;
+					win = "A";
+				}
+			} else {
+				if(!win.equals("B")) {
+					res++;
+					win = "B";
+				}
+			}
+		}
+		System.out.println(res);
+    }
+}
