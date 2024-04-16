525445cb9d578ddfecd88cd1483d34399dc064dd
jjeoungS2 <95362886+jjeoungS2@users.noreply.github.com>
2024-04-16 17:19:27 +0900

    Title: 순위 경쟁, Time: 88ms, Memory: 8MB, Status: Wrong Answer - CodeTree

---
+++ 240416/순위 경쟁/ranking-competition.java
@@ -0,0 +1,55 @@
+import java.util.*;
+import java.io.*;
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		Map<Character, Integer> scores = new HashMap<>();
+		scores.put('A', 0);
+		scores.put('B', 0);
+		scores.put('C', 0);
+		
+		String win = "";
+		int res = 0;
+		
+		for(int i = 0; i < n; i++) {
+			String[] input = br.readLine().split(" ");
+			char who = input[0].charAt(0);
+			int num = Integer.parseInt(input[1]);
+			
+			scores.put(who, scores.get(who) + num);
+			
+			String leader = findLeaders(scores);
+			
+			if(!leader.equals(win)) {
+				win = leader;
+				res++;
+			}
+		}
+		
+		System.out.println(res);
+    }
+
+    private static String findLeaders(Map<Character, Integer> scores) {
+        int maxScore = Integer.MIN_VALUE;
+        StringBuilder leaders = new StringBuilder();
+        
+        // 최대 점수 찾기
+        for (Integer score : scores.values()) {
+            if (score > maxScore) {
+                maxScore = score;
+            }
+        }
+        
+        // 최대 점수를 가진 학생 찾기
+        for (Map.Entry<Character, Integer> entry : scores.entrySet()) {
+            if (entry.getValue() == maxScore) {
+                leaders.append(entry.getKey());
+            }
+        }
+        
+        return leaders.toString();
+    }
+}
